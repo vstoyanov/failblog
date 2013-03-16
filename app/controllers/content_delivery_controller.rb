89526d655bfa64932b6373b9118d92d15270ac41
Vasil Stoyanov <vassilxstoyanov@gmail.com>
2013-03-17 00:14:54 +0100

    content delivery controller selects a suitable publication

--- app/controllers/content_delivery_controller.rb
+++ app/controllers/content_delivery_controller.rb
@@ -1,21 +1,12 @@
 class ContentDeliveryController < ApplicationController
+
   def deliver
     publication_id = params[:publication]
-
-    if publication_id.blank?
-      @publication = Publication.where(:parent_id =>  null)
-    elsif publication_id.instance_of? Numeric
-      @publication = Publication.find(publication_id)
-    elsif publication_id.instance_of? String
-      @publication = Publication.where("title LIKE ?", "%#{@publication_id}%")
-
-    else
-      # No fuckin idea what to do => 404 - point to index
-    end
+    @publication = Publication.find_by_soft_id(publication_id)
 
     respond_to do |format|
-      format.html  render :template => @publication.list_page? ?
-          'content_delivery/display_blog' : 'content_delivery/display_plain'
+      format.html  {render :template => @publication.list_page? ?
+          'content_delivery/display_blog' : 'content_delivery/display_plain'}
       format.json { render json: @publication }
     end
 
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -10,5 +10,23 @@ class Publication < ActiveRecord::Base
   attr_accessible :content, :title, :list_page, :comments_allowed, :live
 
   validates_presence_of :content, :title
+  class << self
+
+    def find_by_soft_id(publication_id)
+
+      if publication_id.to_i.to_s == publication_id
+        publication = Publication.find(publication_id)
+
+      elsif !publication_id.blank?
+        publication = Publication.where("title LIKE ?", "%#{@publication_id}%")
+
+      else
+          publication = Publication.where("my_id IS ?", nil)
+      end
+
+      publication
+    end
+
+  end
 
 end
