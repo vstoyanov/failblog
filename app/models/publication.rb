089c0b4061254a1806a7764942b6f78d0b437577
Vasil Stoyanov <vassilxstoyanov@gmail.com>
2013-03-16 21:51:25 +0100

    changed model to support hierarchical publicationsl

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,8 +1,14 @@
 class Publication < ActiveRecord::Base
-  has_many :comments
-  has_and_belongs_to_many :categories
-  attr_accessible :content, :title
-  validates_presence_of :content, :title
+  belongs_to :parent, :class_name => 'Publication', :foreign_key => 'parent_id'
 
+  has_many :children, :class_name => 'Publication', :foreign_key => 'parent_id'
+
+  has_many :comments
+
+  has_and_belongs_to_many :categories
+
+  attr_accessible :content, :title, :list_page, :comments_allowed, :live
+
+  validates_presence_of :content, :title
 
 end
--- db/migrate/20130128001834_create_publications.rb
+++
@@ -1,9 +0,0 @@
-class CreatePublications < ActiveRecord::Migration
-  def change
-    create_table :publications do |t|
-      t.string :title
-      t.string :content
-      t.timestamps
-    end
-  end
-end
